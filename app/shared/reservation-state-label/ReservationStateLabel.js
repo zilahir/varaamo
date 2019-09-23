f2a2d9f3febf732ca517c90366ae0e64e8e33cfd
Jimi <jiripim@gmail.com>
2019-09-23 11:56:48 +0300

    Add support for new waiting_for_payment status

--- app/shared/reservation-state-label/ReservationStateLabel.js
+++ app/shared/reservation-state-label/ReservationStateLabel.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import includes from 'lodash/includes';
 
 import constants from '../../constants/AppConstants';
 import Label from '../label/Label';
 import injectT from '../../i18n/injectT';
 
-function ReservationStateLabel({ reservation, t }) {
-  if (!reservation.needManualConfirmation && reservation.state !== 'cancelled') {
+// Visibility of label for normal vs payment reservations are a bit different.
+// For payment related reservations just show all states.
+const shouldSkipStateLabelRendering = (reservation, isPayment) => {
+  return (
+    !isPayment
+    && !reservation.needManualConfirmation
+    && includes(['confirmed', 'denied', 'requested'], reservation.state)
+  );
+};
+
+const getLabelData = state => constants.RESERVATION_STATE_LABELS[state];
+
+function ReservationStateLabel({ reservation, isPayment = false, t }) {
+  const data = getLabelData(reservation.state);
+  if (!data || shouldSkipStateLabelRendering(reservation, isPayment)) {
     return <span />;
   }
-  const { labelBsStyle, labelTextId } = constants.RESERVATION_STATE_LABELS[reservation.state];
 
+  const { labelBsStyle, labelTextId } = data;
   return (
     <div className="reservation-state-label-container">
       <Label bsStyle={labelBsStyle}>{t(labelTextId)}</Label>
@@ -24,6 +38,7 @@ ReservationStateLabel.propTypes = {
     state: PropTypes.string.isRequired,
   }).isRequired,
   t: PropTypes.func.isRequired,
+  isPayment: PropTypes.bool.isRequired,
 };
 
 export default injectT(ReservationStateLabel);
